b23bc92068418a29bc81cd84b50602b5ee4413af
opet <opet@Academico.edu>
2018-04-26 09:24:08 -0300

    26/04/2018 09h24

---
+++ ToDo/ToDoMvc/Controllers/ManageUsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TodoMvc.Models;
+using TodoMvc.Models.View;
+
+namespace TodoMvc.Controllers
+{
+    [Authorize(Roles = "Adminstrator")]
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ManageUsersController(UserManager<ApplicationUser>userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var admins = await _userManager.GetUsersInRoleAsync("Administrator");
+
+            var everyone = await _userManager.Users.ToArrayAsync();
+
+            var model = new ManageUsersViewModel
+            {
+                Administrators = admins,
+                everyone = everyone
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ ToDo/ToDoMvc/Models/View/ManageUsersViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoMvc.Models.View
+{
+    public class ManageUsersViewModel
+    {
+        public IEnumerable<ApplicationUser> Administrators { get; set; }
+
+        public IEnumerable<ApplicationUser> Everyone { get; set; }
+    }
+}
