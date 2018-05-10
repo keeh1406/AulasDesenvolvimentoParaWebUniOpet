1f118efed9ee44d9e7ec65db5da7d0baea8ba904
opet <opet@Academico.edu>
2018-05-10 10:44:27 -0300

    10/05/2018 10h45

---
+++ ToDo/ToDo.UnitTests/ToDoItemServiceShould.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoMvc.Data;
+using TodoMvc.Models;
+using TodoMvc.Services;
+using Xunit;
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class TodoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+     .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            // Set up a context (connection to the "DB") for writing
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new ToDoItemService(context);
+
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new NewToDoItem
+                {
+                    Title = "Testing?"
+                }, fakeUser);
+            }
+        }
+    }
+}
